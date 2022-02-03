d273257cf471f05607d02b1f167b690f61fa708c
aaron-truong <47168113+aaron-truong@users.noreply.github.com>
2022-02-03 12:09:23 -0800

    fixed test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void testAdd() {
-        assertEquals(3, SkillDemo.add(1, 1));
+        assertEquals(2, SkillDemo.add(1, 1));
     }
 }
